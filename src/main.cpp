8d0f4cd2941e311bd35d6c87cf9ad44b2699713d
Carlos Rabelo <developer@carlosrabelo.com.br>
2018-09-03 21:20:00 -0400

    feat: read DHT11 and print readings on serial
    Replace the Serial stub with GPIO2 sampling, checksum/timeout handling, and a 60s interval so the firmware is a local monitor before any network code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,74 @@
-#include <Arduino.h>
+#include "dht11.h"
+
+#define DHT_PIN 2
+#define READING_INTERVAL 60000
+
+static void printSystemInfo() {
+    Serial.print("Free heap: ");
+    Serial.println(ESP.getFreeHeap());
+    Serial.print("Chip ID: ");
+    Serial.println(ESP.getChipId());
+    Serial.print("Flash size: ");
+    Serial.println(ESP.getFlashChipSize());
+}
 
 void setup() {
     Serial.begin(115200);
     delay(100);
 
     Serial.println();
-    Serial.println("ESP-01 ready");
+    Serial.println("=== ESP01 DHT11 Monitor ===");
+    printSystemInfo();
 }
 
 void loop() {
+    Serial.println();
+    Serial.println("--- Reading DHT11 sensor ---");
+
+    dht11 dht_sensor;
+    int result = dht_sensor.read(DHT_PIN);
+
+    if (result != DHTLIB_OK) {
+        Serial.print("DHT11 error: ");
+        switch (result) {
+            case DHTLIB_ERROR_CHECKSUM:
+                Serial.println("Checksum mismatch");
+                break;
+            case DHTLIB_ERROR_TIMEOUT:
+                Serial.println("Timeout waiting for sensor");
+                break;
+            default:
+                Serial.println("Unknown error");
+                break;
+        }
+        delay(5000);
+        return;
+    }
+
+    int temperature = dht_sensor.temperature;
+    int humidity = dht_sensor.humidity;
+
+    if (temperature < 0 || temperature > 50 || humidity > 100) {
+        Serial.println("Warning: Invalid sensor reading detected");
+        Serial.print("Temp: ");
+        Serial.print(temperature);
+        Serial.print(", Humidity: ");
+        Serial.println(humidity);
+        delay(5000);
+        return;
+    }
+
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.print("C, Humidity: ");
+    Serial.print(humidity);
+    Serial.println("%");
+
+    printSystemInfo();
+    Serial.print("Next reading in ");
+    Serial.print(READING_INTERVAL / 1000);
+    Serial.println(" seconds...");
+    Serial.println();
+
+    delay(READING_INTERVAL);
 }
